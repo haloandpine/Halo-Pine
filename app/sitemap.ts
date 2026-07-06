30d2e41fe7e61b98ffc3fef7e021ab74d6a6d96a
nimaadiidar <youremail@example.com>
2026-07-06 15:39:23 -0700

    Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,36 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://haloandpine.ca",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://haloandpine.ca/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://haloandpine.ca/services",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: "https://haloandpine.ca/planner",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://haloandpine.ca/contact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+  ];
+}
